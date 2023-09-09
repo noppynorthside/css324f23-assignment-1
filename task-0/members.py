13f08170c7e2b19c1ce28f3865b271388030d2f3
nopeisawesome <nopeisawesome@gmail.com>
2023-09-09 18:59:20 +0700

    Adding Name

--- task-0/members.py
+++ task-0/members.py
@@ -1,4 +1,4 @@
 [
-    {"name": "Cholwich Nattee", "sid": "6422771111"},
-    {"name": "Cholwich Nattee", "sid": "6422771111"},
+    {"name": "Amal Topa", "sid": "6422772234"},
+    {"name": "Noppharut Pipopsophonchai", "sid": "6422781292"},
 ]
